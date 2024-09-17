8928c1701082942ae77f14806e25ecd37c82dbe2
innerstella <suji0509@naver.com>
2024-09-17 14:25:03 +0900

    feat: 비동기 오류 처리

--- app.js
+++ app.js
@@ -23,57 +23,82 @@ mongoose
   .connect(process.env.DATABASE_URL)
   .then(() => console.log("Connected to DB"))
 
-app.get("/soil-data", async (req, res) => {
-  const name = req.query.name
-  const soilData = await Soil.find({ name: name })
-
-  if (soilData.length > 0) {
-    res.status(200).send(soilData)
-  } else {
-    res.status(404).send({ message: "Cannot find soil data." })
+const asyncHandler = (handler) => {
+  return async function (req, res) {
+    try {
+      await handler(req, res)
+    } catch (error) {
+      console.log(`${error.name} : ${error.message}`)
+    }
   }
-})
+}
 
-app.get("/chemical-data", async (req, res) => {
-  const constituent = req.query.constituent
-  const chemicalData = await Chemical.find({ constituent: constituent })
+app.get(
+  "/soil-data",
+  asyncHandler(async (req, res) => {
+    const name = req.query.name
+    const soilData = await Soil.find({ name: name })
 
-  if (chemicalData.length > 0) {
-    res.status(200).send(chemicalData)
-  } else {
-    res.status(404).send({ message: "Cannot find chemical data." })
-  }
-})
+    if (soilData.length > 0) {
+      res.status(200).send(soilData)
+    } else {
+      res.status(404).send({ message: "Cannot find soil data." })
+    }
+  })
+)
 
-app.get("/exposure-data", async (req, res) => {
-  const name = req.query.name
-  const exposureData = await Exposure.find({ name: name })
+app.get(
+  "/chemical-data",
+  asyncHandler(async (req, res) => {
+    const constituent = req.query.constituent
+    const chemicalData = await Chemical.find({ constituent: constituent })
 
-  if (exposureData.length > 0) {
-    res.status(200).send(exposureData)
-  } else {
-    res.status(404).send({ message: "Cannot find exposure data." })
-  }
-})
+    if (chemicalData.length > 0) {
+      res.status(200).send(chemicalData)
+    } else {
+      res.status(404).send({ message: "Cannot find chemical data." })
+    }
+  })
+)
 
-app.post("/c-risk", (req, res) => {
-  const scenario = req.body.scenario
-  const source = req.body.source
-  const pathway = req.body.pathway
-  const receptor = req.body.receptor
+app.get(
+  "/exposure-data",
+  asyncHandler(async (req, res) => {
+    const name = req.query.name
+    const exposureData = await Exposure.find({ name: name })
 
-  const C_Risk = computeCRisk(scenario, source, pathway, receptor)
+    if (exposureData.length > 0) {
+      res.status(200).send(exposureData)
+    } else {
+      res.status(404).send({ message: "Cannot find exposure data." })
+    }
+  })
+)
 
-  res.status(200).send({ C_Risk })
-})
+app.post(
+  "/c-risk",
+  asyncHandler((req, res) => {
+    const scenario = req.body.scenario
+    const source = req.body.source
+    const pathway = req.body.pathway
+    const receptor = req.body.receptor
 
-app.post("/nc-risk", (req, res) => {
-  const scenario = req.body.scenario
-  const source = req.body.source
-  const pathway = req.body.pathway
-  const receptor = req.body.receptor
+    const C_Risk = computeCRisk(scenario, source, pathway, receptor)
 
-  const NC_Risk = computeNCRisk(scenario, source, pathway, receptor)
+    res.status(200).send({ C_Risk })
+  })
+)
 
-  res.status(200).send({ NC_Risk })
-})
+app.post(
+  "/nc-risk",
+  asyncHandler((req, res) => {
+    const scenario = req.body.scenario
+    const source = req.body.source
+    const pathway = req.body.pathway
+    const receptor = req.body.receptor
+
+    const NC_Risk = computeNCRisk(scenario, source, pathway, receptor)
+
+    res.status(200).send({ NC_Risk })
+  })
+)
